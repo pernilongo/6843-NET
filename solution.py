9ec5e79ebb235672d4e56a29fe90bd1f645df967
Juan Rovirosa <juan.rovirosa@gmail.com>
2021-09-14 12:56:52 -0700

    updates by sonar quebe

--- solution.py
+++ solution.py
@@ -2,59 +2,59 @@ from socket import *
 import sys
 
 
-def readFile(filename):
+def read_file(filename):
 	f = open(filename)
 	outputdata = f.read()
 	f.close()
 	return outputdata
 
-def send(connectionSocket, msg):
+def send(connection_socket, msg):
 	size = len(msg)
-	sent = connectionSocket.send(msg.encode())
+	sent = connection_socket.send(msg.encode())
 	print(msg, size, sent)
 
-def handleConnection(connectionSocket, addr):
+def handle_connection(connection_socket, addr):
 	try:
-		message = connectionSocket.recv(4096)
+		message = connection_socket.recv(4096)
 		filename = message.split()[1]
 		try:
-			outputdata = readFile(filename[1:])
+			outputdata = read_file(filename[1:])
 			#Send one HTTP header line into socket.
 			print(filename[1:], len(outputdata))
-			send(connectionSocket, "HTTP/1.1 200 OK\r\n")
-			send(connectionSocket, ("Content-Length: " + str(len(outputdata))) + "\r\n")
-			send(connectionSocket, "\r\n")
+			send(connection_socket, "HTTP/1.1 200 OK\r\n")
+			send(connection_socket, ("Content-Length: " + str(len(outputdata))) + "\r\n")
+			send(connection_socket, "\r\n")
 			#Send the content of the requested file to the client
-			send(connectionSocket, outputdata)
+			send(connection_socket, outputdata)
 		except IOError as error:
 			print(error)
 			# Send response message for file not found (404)
-			send(connectionSocket, "HTTP/1.1 404 File Not Found\r\n")
+			send(connection_socket, "HTTP/1.1 404 File Not Found\r\n")
 
 		#Close client socket
-		connectionSocket.close()
+		connection_socket.close()
 		print('Sent')
 	except (ConnectionResetError, BrokenPipeError) as ex:
 		print(ex)
 
 
-def webServer(port=13331):
-	serverSocket = socket(AF_INET, SOCK_STREAM)
+def web_server(port=13331):
+	server_socket = socket(AF_INET, SOCK_STREAM)
 
 	#Prepare a server socket
-	serverSocket.bind(("", port))
+	server_socket.bind(("", port))
 
-	serverSocket.listen()
+	server_socket.listen()
 
 	while True:
 		#Establish the connection
 		print('Ready to serve...')
-		connectionSocket, addr = serverSocket.accept()
-		handleConnection(connectionSocket, addr)
+		connection_socket, addr = server_socket.accept()
+		handle_connection(connection_socket, addr)
 
 	serverSocket.close()
 	sys.exit()	# Terminate the program after sending the corresponding data
 
 
 if __name__ == "__main__":
-	webServer(13331)
+	web_server(13331)
