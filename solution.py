ac956d66e54e5c6ac3f25266b772b738b74bc88b
Juan Rovirosa <juan.rovirosa@gmail.com>
2021-09-19 09:08:33 -0700

    first summition

--- solution.py
+++ solution.py
@@ -2,9 +2,10 @@
 from socket import *
 
 def assert_response(resp, code):
-	print(resp)
+	#print(resp)
 	if resp[:3] != code:
 		print(code, 'reply not received from server.')
+
 
 def send_cmd(cmd, client_socket):
 	cmd = cmd + '\r\n'
@@ -12,35 +13,37 @@ def send_cmd(cmd, client_socket):
 	resp = client_socket.recv(1024).decode()
 	return resp
 
+
 def send_email(from_addr, to_addrs, msg, client_socket):
 	assert_response(send_cmd('MAIL FROM: ' + from_addr, client_socket), '250')
 	assert_response(send_cmd('RCPT TO: ' + to_addrs, client_socket), '250')
 	assert_response(send_cmd('DATA', client_socket), '354')
-	send_cmd(msg, client_socket)
+	msg = msg + '\r\n'
+	client_socket.send(msg.encode())
 	assert_response(send_cmd('.', client_socket), '250')
 
 
 def smtp_client(port=1025, mailserver='127.0.0.1'):
-	msg = "\r\n My message"
-	endmsg = "\r\n.\r\n"
 
 	# Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
 
 	# Create socket called clientSocket and establish a TCP connection with mailserver and port
 
-	# Fill in start
-	# Fill in end
+	client_socket = socket(AF_INET, SOCK_STREAM)
+	client_socket.connect((mailserver, port))
 
-	resp = clientSocket.recv(1024).decode()
+	resp = client_socket.recv(1024).decode()
 	assert_response(resp, '220')
 
 	# Send HELO command and print server response.
-	assert_response(send_cmd('HELO Alice', clientSocket), '250')
+	assert_response(send_cmd('HELO localhost', client_socket), '250')
 
-	send_email('juan.rovirosa@nyu.edu', 'juan.rovirosa@gmail.com', 'Testing SMTP client program', clientSocket)
+	send_email('juan.rovirosa@nyu.edu', 'rp1912@nyu.edu', 'Testing SMTP client program', client_socket)
 
 	# Send QUIT command and get server response.
-	assert_response(send_cmd('QUIT', clientSocket), '250')
+	assert_response(send_cmd('QUIT', client_socket), '221')
+
+	client_socket.close()
 
 
 if __name__ == '__main__':
